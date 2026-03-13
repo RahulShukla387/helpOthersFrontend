4de2e543723a5b18cc6d6e4a9c108059a1d5a57c
RahulShukla387 <KnowMeTechnical@gmail.com>
2026-03-14 00:22:24 +0530

    adding baseUrl

--- Ngo/src/api/api.js
+++ Ngo/src/api/api.js
@@ -1,4 +1,5 @@
 import axios from "axios";
 export let api = axios.create({
-    baseURL: "http://localhost:8080/api/",
+    // baseURL: "http://localhost:8080/api/",
+    baseURL: "https://myprojectadiyuvanbackend-production.up.railway.app/api/",
 })
