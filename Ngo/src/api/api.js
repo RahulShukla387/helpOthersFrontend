6ddf3977ff30dfc366aabdfaca51f94c1277718c
RahulShukla387 <KnowMeTechnical@gmail.com>
2026-06-26 22:42:01 +0530

    adding backend aws url

--- Ngo/src/api/api.js
+++ Ngo/src/api/api.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 export let api = axios.create({
     // baseURL: "http://localhost:8080/api/",
-    baseURL: "https://myprojectadiyuvanbackend-production.up.railway.app/api/",
+    // baseURL: "https://myprojectadiyuvanbackend-production.up.railway.app/api/",
+    //Url of aws backend
+    baseURL: "https://api.helpothers.space/api/",
 })
